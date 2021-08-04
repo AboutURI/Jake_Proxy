c65689e4afd270591b900193b75bc6f80188c529
Jacob Stevens <J.stevensOne@gmail.com>
2021-08-03 23:59:14 -0400

    added k6

---
+++ k6proxy.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+
+const URL = 'http://localhost:6012/?courseid=12';
+
+export const options = {
+  scenarios: {
+    // eslint-disable-next-line camelcase
+    Constant_Rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s',
+      duration: '90s',
+      preAllocatedVUs: 100,
+      maxVUs: 220,
+    }
+  },
+};
+
+const getRandom = (max) => {
+  return (Math.floor(Math.random() * max));
+};
+
+export default function () {
+  const id = getRandom(10000000);
+  http.get(URL + id);
+}
